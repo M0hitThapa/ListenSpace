2fd5c731c2d791c78d56c3efdb6819dea1f534a0
Mohit Thapa <thapamohit2058@gmail.com>
2025-04-06 23:35:52 +0545

    add authentication

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,49 +1,83 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useForm, SubmitHandler} from "react-hook-form"
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { PEOPLES_IMAGES } from "@/images"
+
+interface FormValues {
+  username:string;
+  name:string;
+}
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+
+  const schema = yup.object().shape({
+    username:yup
+    .string()
+    .required("Username is Required")
+    .matches(/^[a-zA-Z0-9_.@$]+$/, "Invalid Username"),
+    name: yup.string().required("Name is Required"),
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+const {username,name} = data;
+
+const response = await fetch("http://localhost:3001/auth/createUser", {
+method:"POST",
+headers: {
+  "Content-Type":"application/json"
+},
+body: JSON.stringify({
+  username,
+  name,
+  image: PEOPLES_IMAGES[Math.floor(Math.random() * PEOPLES_IMAGES.length)],
+}),
+});
+if(!response.ok) {
+  alert("some error occures while signin");
+  return;
+}
+const responseData = await response.json()
+console.log(responseData)
+  }
+
+  const {register, handleSubmit,formState: {errors}, } = useForm<FormValues>({ resolver:yupResolver(schema)})
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">SignIn your account</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
+          Enter your Name below to login to your account
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Label htmlFor="name">UserName</Label>
+          <Input id="username" type="username" placeholder="Enter Your username" required {...register("username")}/>
+          {errors.username && <p className="text-red-500">{errors.username.message}</p>}
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
+            <Label htmlFor="password">Name</Label>
+          
           </div>
-          <Input id="password" type="password" required />
+          <Input id="name" type="name" required {...register("name")} />
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         </div>
         <Button type="submit" className="w-full">
-          Login
+          SignIn
         </Button>
        
        
       </div>
-      <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="/register" className="underline underline-offset-4">
-          Sign up
-        </a>
-      </div>
+     
     </form>
   )
 }
---
+++ src/images.ts
@@ -0,0 +1,8 @@
+export const PEOPLES_IMAGES= [
+    "https://i.pinimg.com/736x/62/7a/8f/627a8f84855f35f7ce2c0c574a2fde64.jpg",
+    "https://i.pinimg.com/736x/03/79/30/03793005e935a2c11d71554121dc5d37.jpg",
+    "https://i.pinimg.com/736x/f7/44/1a/f7441afe5fa6c2f7ff022614463b476e.jpg",
+    "https://i.pinimg.com/736x/65/92/11/659211169ae2c53755904c590b0a6fc9.jpg",
+    "https://i.pinimg.com/736x/8f/1c/d1/8f1cd1c398ca68c4403038b8dfa20f0a.jpg",
+    "https://i.pinimg.com/736x/b7/88/79/b78879ce82b422bbf708956a583a777e.jpg",
+];
